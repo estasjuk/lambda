74700b2c4a325a4af1e06f044433721d4aba2ee7
Olena Stasiuk <estasjuk@gmail.com>
2023-05-29 14:10:53 +0300

    add elegramConsoleSender

---
+++ telegramConsoleSender/app.js
@@ -0,0 +1,40 @@
+const TelegramBot = require('node-telegram-bot-api');
+const { program } = require('commander');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { token, chatId } = process.env;
+
+const bot = new TelegramBot(token, { polling: true });
+
+program.version('1.0.0');
+
+program
+    .command('message')
+    .description('send message')
+    .alias('m')
+    .argument('<message>')
+    .action(async (msg) => {
+        await bot.sendMessage(chatId, msg);
+        process.exit(0);
+    }
+  );
+
+program
+  .command('photo')
+  .description('send photo')
+  .alias('p')
+  .argument('<path>')
+  .action(async (photo) => {
+      await bot.sendPhoto(chatId, photo);
+      process.exit(0);
+  });
+
+program
+  .option('-m, --message', 'for sending message')
+  .option('-p, --photo', 'for sending photo')
+  .action(task => {
+    console.log(`${task}`);
+  });
+
+program.parse();
