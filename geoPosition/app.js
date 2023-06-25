e1ebc1b03eca501fff6b3018ef324084d7ef0944
Olena Stasiuk <estasjuk@gmail.com>
2023-06-26 00:22:48 +0300

    add geoPosition

---
+++ geoPosition/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+require('dotenv').config();
+const ngrok = require('ngrok');
+
+const { formatIp, searchIp } = require('./services');
+const { PORT, NGROK_TOKEN } = process.env
+const app = express();
+
+const server = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log('Server is running...');
+    });
+    const url = await ngrok.connect({
+      addr: PORT,
+      authtoken: NGROK_TOKEN,
+    });
+    console.log('ngrok URL:', url);
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+app.set('trust proxy', true);
+
+app.get('/', (req, res) => {
+  const ip = formatIp(req.ip);
+  const object = searchIp(ip);
+  res.json({
+    IP: ip,
+    countryCode: object.code,
+    country: object.country,
+})
+});
+
+server();
---
+++ geoPosition/services.js
@@ -0,0 +1,32 @@
+const { readFileSync } = require('fs');
+
+//transform input data:
+const arrayOfIpLocations = readFileSync('C:/Users/Stasyuk.Olena/Desktop/lambda/geoPosition/ipLocations.CSV', 'utf8').trim().split("\r\n")
+const formattedArrayOfIpLocations = arrayOfIpLocations.map(item => item.split(";"));
+const arrayOfObjects = formattedArrayOfIpLocations.map(item => {
+    return {
+        firstIp: Number(item[0]),
+        lastIp: Number(item[1]),
+        code: item[2],
+        country: item[3],
+    }
+    }
+);
+
+//transform IP-adress:
+const formatIp = ip => {
+  const parts = ip.split(".");
+  const res = parts.reduce((acc, part, index) => {
+    return acc + Number(part) * Math.pow(256, 3 - index)
+  }, 0)
+  return res;
+};
+
+const searchIp = ip => {
+  return arrayOfObjects.find(item => item.firstIp <= ip && item.lastIp >= ip)
+};
+
+module.exports = {
+    formatIp,
+    searchIp,
+}
