df32774ca171d3181afc56f9bbded014e1efb30c
Olena Stasiuk <estasjuk@gmail.com>
2023-08-02 19:13:38 +0300

    add cryptoCurrencyApi

---
+++ cryptocurrencyAPI/routes/cryptoRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+// router.get('/', authenticate, getAllContacts);
+// router.get('/:contactId', authenticate, isValidId, getContactById);
+// router.post('/', authenticate, validateBody(addSchema), addContact );
+// router.put('/:contactId', authenticate, validateBody(updateSchema), updateContact);
+// router.patch('/:contactId/favorite', authenticate, validateBody(updateFavoriteSchema), updateStatusContact);
+// router.delete('/:contactId', authenticate, removeContact);
+
+module.exports = router;
---
+++ cryptocurrencyAPI/server.js
@@ -0,0 +1,14 @@
+const app = require('./app')
+const mongoose = require('mongoose')
+
+const {PORT = 3000, DB_HOST} = process.env;
+
+mongoose.connect(DB_HOST)
+.then(console.log("Database connection successful"))
+.then(app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`)
+}))
+.catch(error => {
+  console.log(error.message);
+  process.exit(1);
+});
